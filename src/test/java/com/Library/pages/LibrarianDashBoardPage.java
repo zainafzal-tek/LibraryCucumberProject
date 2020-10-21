6095a5280f31e8fba46f7fec894d25232b2b782b
zainafzal-tek <zainafzal@gmail.com>
2020-10-21 17:20:54 -0400

    update project

---
+++ src/test/java/com/Library/pages/LibrarianDashBoardPage.java
@@ -0,0 +1,28 @@
+package com.Library.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LibrarianDashBoardPage {
+
+    @FindBy(xpath = "//div/h6[.='Users']")
+    private WebElement userHeader;
+
+    @FindBy(xpath = "//div/h6[.='Books']")
+    private WebElement booksHeader;
+
+    @FindBy(xpath = "//div/h6[.='Borrowed Books']")
+    private WebElement borrowedBooksHeader;
+
+    @FindBy(xpath = "//a/span[.='Dashboard']")
+    private WebElement dashboardLink;
+
+    @FindBy(xpath = "//a/span[.='Users']")
+    private WebElement userLink;
+
+    @FindBy(xpath = "//a/span[.='Books']")
+    private WebElement booksLink;
+
+    public void
+
+}
