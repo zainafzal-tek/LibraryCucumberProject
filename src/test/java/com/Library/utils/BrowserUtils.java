093548844d105cec6b3c0cf23203e8430720fbcd
zainafzal-tek <zainafzal@gmail.com>
2020-10-26 21:57:52 -0400

    update library

--- src/test/java/com/Library/utils/BrowserUtils.java
+++ src/test/java/com/Library/utils/BrowserUtils.java
@@ -1,13 +1,45 @@
 package com.Library.utils;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BrowserUtils {
 
+    private static WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 20);
+
+
     public static Select getSelectDropdown(WebElement element){
+        wait.until(ExpectedConditions.visibilityOf(element));
         Select select = new Select(element);
         return select;
+    }
+
+    public static void wait(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void clickOnElement(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+    }
+
+    public static void clickWithJS(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        ((JavascriptExecutor) (Driver.getDriver())).executeScript("arguments[0].click()", element);
+    }
+
+    public static void enterText(WebElement element, String text){
+        wait.until(ExpectedConditions.visibilityOf(element));
+        element.clear();
+        element.sendKeys(text);
+        wait.until(ExpectedConditions.attributeToBe(element, "value", text));
+        System.out.println("Entering text: " + text);
     }
 
 }
