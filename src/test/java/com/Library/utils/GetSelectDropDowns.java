5ec995c695ff41d48c00d2b76d8d4b05151a7296
zainafzal-tek <zainafzal@gmail.com>
2020-11-03 17:13:10 -0500

    update library

--- src/test/java/com/Library/utils/GetSelectDropDowns.java
+++ src/test/java/com/Library/utils/GetSelectDropDowns.java
@@ -4,6 +4,6 @@ import org.openqa.selenium.support.ui.Select;
 
 public interface GetSelectDropDowns {
 
-     abstract Select getSelectDropDown(String dropDownName);
+     Select getSelectDropDown(String dropDownName);
 
 }
