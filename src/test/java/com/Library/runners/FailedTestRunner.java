ed3344fee5fbe39d47c91a1c8bcd24234074acde
RGallardo <ragg86@gmail.com>
2020-11-10 00:18:27 -0500

    Create FailedTestRunner.java
    to re run failed tests

---
+++ src/test/java/com/Library/runners/FailedTestRunner.java
@@ -0,0 +1,13 @@
+package com.Library.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt"
+        ,glue =  "com/library/step_definitions"
+)
+
+public class FailedTestRunner { }
