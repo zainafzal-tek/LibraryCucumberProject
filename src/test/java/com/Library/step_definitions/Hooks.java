3be287fa79cfac534bc6d10595b86d46b59d4de8
zainafzal-tek <zainafzal@gmail.com>
2020-11-10 03:14:34 -0500

    added SS to after Hook

--- src/test/java/com/Library/step_definitions/Hooks.java
+++ src/test/java/com/Library/step_definitions/Hooks.java
@@ -5,6 +5,9 @@ import com.Library.pages.LoginPage;
 import com.Library.utils.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import java.util.concurrent.TimeUnit;
 
@@ -18,7 +21,13 @@ public class Hooks {
     }
 
     @After
-    public void tearDown(){
+    public void tearDown(Scenario scenario){
+        //this is a hook after
+        //runs automatically after every test
+        if (scenario.isFailed()){
+            byte[] data = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(data, "image/png", scenario.getName());
+        }
         Driver.closeDriver();
         System.out.println(":::Step Complete:::");
     }
